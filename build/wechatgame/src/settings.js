496cd522534fae3161d03c2bc6a233b4ee32c618
caohanzong <82314075+caohanzong@users.noreply.github.com>
2021-06-03 20:26:58 +0800

    增加了微信的build文件

---
+++ build/wechatgame/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"wechatgame",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"",server:"",jsList:[],bundleVers:{internal:"ed355",main:"bcb1c"}};
